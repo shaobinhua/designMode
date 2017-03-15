a9438293af01a413b14ef9f842d96a7eb73401bb
shaobinhua <596153668@qq.com>
2017-03-16 00:50:31 +0800

    user enum

--- src/com/binhua/singleton/useenum/Singleton.java
+++ src/com/binhua/singleton/useenum/Singleton.java
@@ -1,18 +1,29 @@
 package com.binhua.singleton.useenum;
 
-/**
+/** 单元素的枚举类型已经成为实现Singleton的最佳方法
  * Created by shaobinhua on 2017/3/16.
  */
 public enum Singleton {
     /**
      * 定义一个枚举的元素，它就代表了Singleton的一个实例。
      */
-    INSTANCE;
+    INSTANCE("jimmy", 1);
 
-    /**
-     * 单例可以有自己的操作
-     */
-    public void singletonOperation(){
-        //功能处理
+    private int id;
+    private String name;
+
+    private Singleton(String name, int id) {
+        this.name = name;
+        this.id = id;
     }
+
+    public static Singleton getInstance() {
+        return INSTANCE;
+    }
+
+    @Override
+    public String toString() {
+        return "name:" + name + " id:" + id;
+    }
+
 }
---
+++ src/com/binhua/singleton/useenum/SingletonTest.java
@@ -0,0 +1,12 @@
+package com.binhua.singleton.useenum;
+
+/**
+ * Created by shaobinhua on 2017/3/16.
+ */
+public class SingletonTest {
+    public static void main(String[] args) {
+        System.out.println(Singleton.INSTANCE);
+        System.out.println(Singleton.INSTANCE);
+
+    }
+}
